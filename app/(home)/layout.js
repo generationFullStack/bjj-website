f9d81086a3dcf8ee601bfdaf6ad1b6d10fd96fec
Gavin <kakitgavinon99@gmail.com>
2025-04-30 02:52:11 +0800

    add route group

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -1,11 +1,10 @@
-import "./globals.css";
-import NavBar from "../components/NavBar";
-import LoadingWrapper from "../components/LoadingWrapper";
-import Footer from "../components/Footer";
+import "../globals.css";
+import NavBar from "../../components/NavBar";
+import LoadingWrapper from "../../components/LoadingWrapper";
+import Footer from "../../components/Footer";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 export const metadata = {
   title: "BJJ",
