7fe525f71eceec34c8058054bc71efb5885ee6d1
Gavin <kakitgavin@gmail.com>
2025-05-07 16:15:44 +0800

    auth

--- actions/action.js
+++ actions/action.js
@@ -1,5 +1,13 @@
 "use server";
 
+import { createSession } from "@/lib/session";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
 export async function signUp(formData) {
   try {
   } catch (error) {
@@ -15,7 +23,22 @@ export async function signUp(formData) {
 export async function submitLoginForm(previousState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
-  console.log(email, password);
+
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      `SELECT * FROM users WHERE email = '${email}' AND password_hash = '${password}'`
+    );
+    client.release();
+
+    if (result.rows.length === 0) {
+      console.log("login failed");
+      return;
+    }
+  } catch (error) {}
+
+  //await createSession(email);
+  console.log("login successful");
 
   return "error!";
 }
