49b82073cebc701df7ecffc7e693233923994ce6
Pan <lauwaipan4@gmail.com>
2025-05-11 21:16:24 +0800

    update by pan

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,12 +2,15 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className="bg-[#151414] py-5">
-      <div className="max-w-[1300px] mx-auto px-5 flex justify-between items-center flex-wrap-reverse text-white">
-        <div className="flex items-center text-lg font-bold">
+      <div
+        className={`max-w-[1300px] mx-auto px-5 flex justify-between items-center flex-wrap-reverse text-white ${styles.footerContainer}`}
+      >
+        <div className="flex items-center text-3xl font-bold">
           <Link href="/" className="flex items-center text-white no-underline">
             <Image
               src="/bjj-letter-logo.png"
@@ -21,34 +24,34 @@ export default function Footer() {
             </span>
           </Link>
         </div>
-        <div className="my-5 flex gap-4">
+        <div className={`my-5 flex gap-10 ${styles.socialMedia}`}>
           <a
             href="#"
-            className="text-white text-2xl hover:text-[#3b5998] transition-colors duration-600"
+            className="text-white text-5xl hover:text-[#3b5998] transition-colors duration-600"
           >
             <i className="fab fa-facebook-f fa-2xs"></i>
           </a>
           <a
             href="#"
-            className="text-white text-2xl hover:text-[#71767b] transition-colors duration-600"
+            className="text-white text-5xl hover:text-[#71767b] transition-colors duration-600"
           >
             <i className="fab fa-x-twitter fa-2xs"></i>
           </a>
           <a
             href="#"
-            className="text-white text-2xl hover:text-[#e1306c] transition-colors duration-600"
+            className="text-white text-5xl hover:text-[#e1306c] transition-colors duration-600"
           >
             <i className="fab fa-instagram fa-2xs"></i>
           </a>
           <a
             href="#"
-            className="text-white text-2xl hover:text-[#ff0000] transition-colors duration-600"
+            className="text-white text-5xl hover:text-[#ff0000] transition-colors duration-600"
           >
             <i className="fab fa-youtube fa-2xs"></i>
           </a>
         </div>
-        <p className="text-[#757575]">
-          BJJ © 2025 All Rights Reserved. Desgin by PAN & JASON & GAVIN
+        <p className="text-[#757575] text-lg font-bold ">
+          BJJ © 2025 All Rights Reserved. Design by PAN & JASON & GAVIN
         </p>
       </div>
     </footer>
