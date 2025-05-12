bad796eaf84a3c07b09d712744571a73827a858d
pannnnl <48431027+pannnnl@users.noreply.github.com>
2025-05-12 22:51:29 +0800

    update signup page by pan

---
+++ app/(home)/signup/page.js
@@ -0,0 +1,10 @@
+// app/signup/page.jsx
+import SignUpForm from "@/components/SignUpForm";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-[#111] w-full overflow-hidden">
+      <SignUpForm />
+    </div>
+  );
+}
