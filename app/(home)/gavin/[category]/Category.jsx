1326e09bc0fd9ca3e3f4f8026e306d42bfaf2c6c
Gavin <kakitgavinon99@gmail.com>
2025-05-12 16:22:53 +0800

    category update

--- app/(home)/gavin/[category]/Category.jsx
+++ app/(home)/gavin/[category]/Category.jsx
@@ -1,51 +1,73 @@
 "use client";
 import { useEffect, useState } from "react";
 
+const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+
 export default function Category({ category }) {
-  let categoryId;
-  switch (category) {
-    case "submissions":
-      categoryId = 20;
-      break;
-
-    case "guard%20passing":
-      categoryId = 19;
-      break;
-
-    case "defense":
-      categoryId = 22;
-      break;
-
-    case "takedown":
-      categoryId = 33;
-      break;
-  }
-
-  const [videoList, setVideoList] = useState([]);
+  const [videoIds, setVideoIds] = useState([]);
+  const [videosData, setVideosData] = useState([]);
 
   useEffect(() => {
     // fetch categories from database to show on the navbar --Gavin
-    async function fetchVideos() {
+    async function fetchVideoIds() {
       try {
         const response = await fetch(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/videos/${categoryId}`
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/videos/${category}`
         );
         const body = await response.json();
-        setVideoList(body);
+        const array = [];
+
+        for (let i = 0; i < body.length; i++) {
+          array.push(body[i].youtube_id);
+        }
+
+        setVideoIds(array);
       } catch (error) {}
     }
-    fetchVideos();
+    fetchVideoIds();
   }, []);
 
-  console.log(videoList);
-  let object = {};
+  useEffect(() => {
+    async function fetchVideosData() {
+      try {
+        const response = await fetch(
+          `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${videoIds.join(
+            ","
+          )}&key=${YOUTUBE_API_KEY}`
+        );
+        const body = await response.json();
 
-  for (let i = 0; i < videoList.length; i++) {
-    let categoryName = videoList[i]["name"];
-    object.categoryName.push(videoList[i]["youtube_id"]);
-  }
+        const array = body.items.map((item) => ({
+          videoId: item.id,
+          title: item.snippet.title,
+        }));
 
-  console.log(object);
+        setVideosData(array);
+      } catch (error) {}
+    }
+    fetchVideosData();
+  }, [videoIds]);
 
-  return <div>{category}</div>;
+  console.log(videosData);
+
+  return (
+    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-50 justify-items-center gap-30 p-10">
+      {videosData.map((element) => (
+        <li
+          key={element.videoId}
+          className="p-5 rounded-2xl bg-white/10 shadow-sm shadow-white/60 transition duration-200 ease-in-out hover:scale-103"
+        >
+          <a href={``}>
+            <h1 className="text-center text-2xl md:text-3xl font-bold w-full h-25 mb-5 text-clip">
+              {element.title}
+            </h1>
+            <img
+              src={`https://img.youtube.com/vi/${element.videoId}/0.jpg`}
+              className="rounded-2xl"
+            />
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 }
